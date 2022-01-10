fbcfdcc21b3dd4e1d7c90c6499b018de91546121
jana.engufpe@gmail.com <jana.engufpe@gmail.com>
2022-01-10 20:22:33 -0300

    lista_pessoas

---
+++ aula_dia10/lista_pessoas.py
@@ -0,0 +1,6 @@
+lista_pessoas =[
+{"nome":"janaina","sobrenome": "santos", "idade": 1996, "peso":58 }
+{"nome":"eduarda","sobrenome": "santos", "idade": 1997, "peso":58 }
+{"nome":"veronica","sobrenome": "santos", "idade": 1994, "peso":65 }
+{"nome":"valdomiro ","sobrenome": "santos", "idade": 1991, "peso":90 }
+]
