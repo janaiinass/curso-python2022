76d6316ce4c59091578149b6d3d047a8b4a7091a
jana.engufpe@gmail.com <jana.engufpe@gmail.com>
2022-01-10 20:17:34 -0300

    lista_pessoas

---
+++ lista_pessoas.py
@@ -0,0 +1,7 @@
+lista_pessoas = [
+    ["Mario", "Carvalho", "Campo Grande", 1997, 85],
+    ["Pedro", "Campos", "Campo Grande", 1994, 78],
+    ["Elisa", "Vitória", "Campo Grande", 2000, 45],
+]
+
+print(lista_pessoas[0])
