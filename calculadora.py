da0565b5c9418483e0a553f2d1f5e165a1124005
jana.engufpe@gmail.com <jana.engufpe@gmail.com>
2022-01-11 20:53:00 -0300

    calculadora_versao_2

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,15 @@
 def soma(num1, num2):
     return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
+
+def mult(num1, num2):
+    return num1 * num2
     
+def div(num1, num2):
+    return num1 / num2
+       
 # Entrada
 print("Escolha qual opeção deseja realizar: ")
 opcao = input("+, -, /, *:")
