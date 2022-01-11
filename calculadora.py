ab5a5c57134c1ee4953bf69f25f3f7417f41fc4f
jana.engufpe@gmail.com <jana.engufpe@gmail.com>
2022-01-11 20:46:54 -0300

    calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,26 @@
+def soma(num1, num2):
+    return num1 + num2
+    
+# Entrada
+print("Escolha qual opeção deseja realizar: ")
+opcao = input("+, -, /, *:")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+if opcao == "+":
+    valor_soma = soma(num1, num2)
+    print(f"{num1} + {num2} == {valor_soma}")   
+
+if opcao == "-":
+    valor_soma = soma(num1, num2)
+    print(f"{num1} + {num2} == {valor_soma}")   
+
+if opcao == "/":
+    valor_soma = soma(num1, num2)
+    print(f"{num1} + {num2} == {valor_soma}")  
+
+if opcao == "*":
+    valor_soma = soma(num1, num2)
+    print(f"{num1} + {num2} == {valor_soma}")    
+    
+
--- imprima_numeros_impares.py
+++ imprima_numeros_impares.py
@@ -1,3 +1,3 @@
 #Faça um programa que imprima na tela apenas os números ímpares entre
-for i in range(1,50,2):
+for i in range(1,50,2): #Na definição da função range() é utilizado a notação de conjuntos numéricos para documentar se o intervalo da sequência é aberto ou fechado.
     print (i)
