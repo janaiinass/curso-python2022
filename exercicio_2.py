7f8480fdb58f805a24088cd0fc9d761c39085d4a
jana.engufpe@gmail.com <jana.engufpe@gmail.com>
2022-01-11 21:19:00 -0300

    exercicio_2

---
+++ exercicio_2.py
@@ -0,0 +1,12 @@
+def soma(x,y,z):
+    res = x + y + z
+    return res
+    
+def menu():
+    x = int(input('Primeiro numero: '))
+    y = int(input('Segundo numero : '))
+    z = int(input('Terceiro numero: '))
+
+    print('Soma: ', soma(x,y,z))
+
+   
