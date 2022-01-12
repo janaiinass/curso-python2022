76dbab050e5febb64c4424f5e31905a043b05488
jana.engufpe@gmail.com <jana.engufpe@gmail.com>
2022-01-11 22:26:15 -0300

    exer_2

--- funcao_tres_argumento.py
+++ funcao_tres_argumento.py
@@ -1,9 +1,10 @@
 #Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
-def soma (num1,num2,num3,):
-    soma=num1 + num2 + num3
+def soma (num1, num2 , num3):
+    soma= num1 + num2 + num3
     return soma 
 
 num1 =float(input("digite o primeiro numero: "))
 num2 =float(input("digite o segundo numero: "))
 num3 =float(input("digite o terceiro numero: "))
-print("soma : ", num1 + num2+ num3)
+result= soma (num1, num2, num3)
+print(result)
