f040ebb29d293567cf6da0e46eda33561ded8ca0
janaina santos <97249394+janaiinass@users.noreply.github.com>
2022-01-11 21:37:40 -0300

    Delete funcao_tres_argumento.py

--- funcao_tres_argumento.py
+++
@@ -1,9 +0,0 @@
-def soma(a,b,c):
-    res = a + b + c
-    return res
-def menu():
-    a = int(input('Primeiro numero: '))
-    b = int(input('Segundo numero : '))
-    c = int(input('Terceiro numero: '))
-
-    print('Soma: ', soma(a,b,c))
