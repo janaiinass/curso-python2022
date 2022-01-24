72ded06c6f9020e2ea34bbbd90b3d9318394be75
jana.engufpe@gmail.com <jana.engufpe@gmail.com>
2022-01-24 00:48:54 -0300

    lista1_2_3

---
+++ listas/lista 1/questao 4.py
@@ -0,0 +1,8 @@
+#faça um programa que peça o raio de um círculo, calcule e mostre a sua área
+
+# float para trabalhar com casas decimais
+raio = float(input("digite o valor do raio para calular a area de um circulo "))
+
+area = 3.14*raio*raio #salvando a do raio area na variavel area
+
+print ("a area é : ", area) 
